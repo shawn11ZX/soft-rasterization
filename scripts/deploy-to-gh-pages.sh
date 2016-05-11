ce0c344461de56d3b2d577bc4e9ea7f963c9ddee
Shawn <sure.zhang@gmail.com>
2016-05-11 20:16:51 +0800

    fix travis

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,16 +2,8 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-rm -rf public
-mkdir public
-
 # config
 git config --global user.email "sure.zhang@gmail.com"
 git config --global user.name "Shawn"
 
-# deploy
-cd public
-git init
-git add .
-git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
